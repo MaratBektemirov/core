e83a36f3dbad33653c0e35f41181c6ec074e457e
Iskandar Gabdrakhmanov <faruk.iskandar@yandex.ru>
2020-08-07 19:49:37 +0300

    script for signing document using rsa key added

---
+++ crypto/sign.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+display_usage() {
+    echo -e "\nUsage:\n$0 <file_to_sign> <private_key>\n" 
+}
+
+file_to_sign=$1
+private_key=$2
+
+if [[ $# -lt 2 ]] ; then
+  display_usage
+  exit 1
+fi
+
+# Sign the file using sha256 digest and PKCS1 padding scheme
+openssl dgst -sha256 -sign $private_key -out /tmp/$file_to_sign.sha256 $file_to_sign
+openssl base64 -A -in /tmp/$file_to_sign.sha256
